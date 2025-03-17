cd43b52ccb20b83ca175382e2bda3fb0a1f67e94
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-03-18 00:58:15 +0530

    improved UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return (
-       <nav className="flex gap-4 container mx-auto  font-bold">
+       <nav className="flex gap-4 container mx-auto p-2 font-bold">
          <Link className="hover:underline" to={'/'}>Home</Link>
          <Link className="hover:underline" to={'/users'}>Users</Link>
+        
        </nav>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from './pages/LandingPage.tsx';
-import UsersPage from './pages/UsersPage.tsx';
-import Navbar from './components/Navbar.tsx';
-
-createRoot(document.getElementById('root')!).render(
+import LandingPage from "./pages/LandingPage.tsx";
+import UsersPage from "./pages/UsersPage.tsx";
+import Navbar from "./components/Navbar.tsx";
+import { ThemeProvider } from "@/components/theme-provider";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-     <Navbar/>
-      <Routes>
-        <Route path='/' element = {<LandingPage/>} />
-        <Route path='/users' element = {<UsersPage/>} />
-      </Routes>
-    </Router>
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/users" element={<UsersPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  </StrictMode>
+);
